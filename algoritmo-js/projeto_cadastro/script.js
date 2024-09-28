3236ae3dc2dad8049ed9edea722c761b5703a904
kevin <kevinup13@gmail.com>
2024-09-28 17:06:43 -0400

    Ajuste de codigo html e css

--- algoritmo-js/projeto_cadastro/script.js
+++ algoritmo-js/projeto_cadastro/script.js
@@ -1,1 +1,14 @@
 console.log("Teste de funcionamento. ok")
+
+// initial data
+peso = document.querySelector("#peso");
+altura = document.querySelector("#altura");
+
+
+console.log(peso);
+console.log(altura);
+
+alert(peso);
+alert(altura);
+// functions
+// Events
